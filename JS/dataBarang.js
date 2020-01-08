ad82ce4dfa9627b478c0a5ad0cc8c8826d9eff93
faJaar <slashcrackdie@gmail.com>
2020-01-08 17:44:03 +0700

    add image produk

--- JS/dataBarang.js
+++ JS/dataBarang.js
@@ -1,5 +1,10 @@
 const Barang ={
     Tas : {
-        
+        nama : 'Tas Carier 40L',
+        harga : '20000',
+    },
+    Tas :{
+        nama : 'Tas 60L',
+        harga : '250000'
     }
 }
